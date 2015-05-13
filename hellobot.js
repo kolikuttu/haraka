0e362c339fd4e962a87d551f3f26c5a3c2749d59
Lohitha Chiranjeewa <lohitha@leapset.com>
2015-05-13 22:57:51 +0530

    edited

--- hellobot.js
+++ hellobot.js
@@ -10,24 +10,23 @@ module.exports = function (req, res, next) {
     return res.status(200).json(botPayload);
   }
 
-  if (userName == 'slackbot' || userName == 'chathuraratanyake' || userName == 'gavinalex' || userName == 'hiranya'){
-    return res.status(200).end();
-  }
+  //if (userName == 'slackbot' || userName == 'chathuraratanyake' || userName == 'gavinalex' || userName == 'hiranya'){
+  //  return res.status(200).end();
+  //}
 
   var date = new Date().getSeconds();
-  if (date<10 || (date>20 && date<30) || (date>40 && date<50)) {
+  //if (date<10 || (date>20 && date<30) || (date>40 && date<50)) {
     var quote = "";
     request('http://www.iheartquotes.com/api/v1/random?format=json', function(err, response, body){
       var resJson = JSON.parse(body);
       quote = resJson.quote;
       botPayload = {
         text : quote
-        //text : userName + ', please update your tickets with the time spent.\nGavin, no need to chase after these guys'
       };
       return res.status(200).json(botPayload);
     });
-  } else {
-    return res.status(200).end();
-  }
+  //} else {
+  //  return res.status(200).end();
+  //}
 
 }
